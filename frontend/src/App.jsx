5739ce30da2521cc6369ed6a40bb2c29da813af6
Sarav <Saravjeetofficial@gmail.com>
2025-08-02 18:02:09 +0530

    routing, toaster, tailwindcss and daisyui setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import {Route, Routes} from "react-router"
+import HomePage from "./pages/HomePage"
+import NoteDetailPage from "./pages/NoteDetailPage"
+import CreatePage from "./pages/CreatePage"
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage/>} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
